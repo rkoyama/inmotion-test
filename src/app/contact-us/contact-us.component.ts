d982ac67f8fae1bda846f932df00d74455b37e22
Ryo Koyama <rkoyama89@gmail.com>
2022-01-17 22:32:49 -0700

    Adding google maps

--- src/app/contact-us/contact-us.component.ts
+++ src/app/contact-us/contact-us.component.ts
@@ -1,15 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, AfterViewInit, ViewChild} from '@angular/core';
+import { Loader } from '@googlemaps/js-api-loader';
+
+const loader = new Loader({
+  apiKey: "AIzaSyCL1BWahUIalM6YnddYj0DtPNOFAoHNlgg",
+  version: "weekly"
+})
 
 @Component({
   selector: 'app-contact-us',
   templateUrl: './contact-us.component.html',
   styleUrls: ['./contact-us.component.scss']
 })
-export class ContactUsComponent implements OnInit {
+export class ContactUsComponent implements OnInit, AfterViewInit {
+  map: any;
+  @ViewChild('mapElement') mapElement: any;
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  ngAfterViewInit(): void {
+    loader.load().then(() => {
+      this.map = new google.maps.Map(this.mapElement.nativeElement, {
+        center: { lat: -34.397, lng: 150.644 },
+        zoom: 8
+      });
+    });
   }
 
 }
